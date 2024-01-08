9260d194c9fa6459bc5614678c6652c0eee565ca
Dawncute <lethibinhminh1307@gmail.com>
2024-01-08 20:59:12 +0700

    nothing

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -18,6 +18,25 @@ const Header = () => {
     setClicked(!clicked);
   };
 
+  const index1 = 1
+  const index2 = 2
+  const index3 = 3
+  const index4 = 4
+  const index5 = 5
+
+  const [underlinedTitles, setUnderlinedTitles] = useState([]);
+
+  const handleTitleClick = (index) => {
+    // Clone mảng để tránh thay đổi trực tiếp state
+    const newUnderlinedTitles = [...underlinedTitles];
+
+    // Đảo ngược trạng thái của tiêu đề được click
+    newUnderlinedTitles[index] = !newUnderlinedTitles[index];
+
+    // Cập nhật state
+    setUnderlinedTitles(newUnderlinedTitles);
+  };
+
   return (
     <nav id="nav">
       <div id="navbar-language">
@@ -35,12 +54,18 @@ const Header = () => {
           <ul id="title" className={clicked ? "active" : "title"}>
             <li>
               <Link to="/">
-                <p>{t("home")}</p>
+                <p
+                  style={{ textDecoration: underlinedTitles[index1] ? 'underline' : 'none' }}
+                  onClick={() => handleTitleClick(index1)}
+                >{t("home")}</p>
               </Link>
             </li>
             <li>
               <Link to="/programs">
-                <p>{t("projects")}</p>
+                <p
+                  style={{ textDecoration: underlinedTitles[index2] ? 'underline' : 'none' }}
+                  onClick={() => handleTitleClick(index2)}
+                >{t("projects")}</p>
               </Link>
             </li>
             <li>
